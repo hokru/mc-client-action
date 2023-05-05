5a060fe125fcbfa66bf11b0a386d04ccb3ac9e77
Holger Kruse <mail2holger@gmail.com>
2023-05-05 15:26:09 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 # instead of mc alias the config is done via MC_HOST_<alias>
 export MC_HOST_$TARGET=https://$ACCESS_KEY:$SECRET_KEY@$ENDPOINT
-
-echo "Running: $1" >> $GITHUB_OUTPUT
-$1
+export CMD=$1
+echo "text=Running: $CMD" >> $GITHUB_OUTPUT
+$CMD

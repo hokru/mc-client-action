f96fac8274d20d340949daffc2e25e15f3a1e1eb
Holger Kruse <mail2holger@gmail.com>
2023-05-08 10:13:45 +0200

    rename target to alias; fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # instead of mc alias the config is done via MC_HOST_<alias>
-export MC_HOST_$TARGET=https://$ACCESS_KEY:$SECRET_KEY@$ENDPOINT
+export MC_HOST_$ALIAS=https://$ACCESS_KEY:$SECRET_KEY@$ENDPOINT
 export CMD=$1
-echo "text=Running: $CMD" >> $GITHUB_OUTPUT
+# echo "text=Running: $CMD" >> $GITHUB_OUTPUT
 $CMD

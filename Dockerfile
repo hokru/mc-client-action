ea74e30222332be6973eb47623223b5aa22a9bfd
Holger Kruse <mail2holger@gmail.com>
2023-05-08 13:42:11 +0200

    make mc act from user space and add working_directory support

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM minio/mc:latest
 
+RUN useradd --user-group --system --create-home --no-log-init mc-client-action
+USER mc-client-action
 ADD entrypoint.sh /entrypoint.sh
+
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 # instead of mc alias the config is done via MC_HOST_<alias>
 export MC_HOST_$ALIAS=https://$ACCESS_KEY:$SECRET_KEY@$ENDPOINT
+
+if [[ -z "$WORKDIR" ]]; then
+    cd $WORKDIR
+fi
 export CMD=$1
 # echo "text=Running: $CMD" >> $GITHUB_OUTPUT
 $CMD
